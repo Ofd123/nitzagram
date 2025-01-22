2fdf2a611ec8923ef9fe14b34b08b39b7ccca764
ad6354 <ad6354@bs.amalnet.k12.il>
2025-01-22 19:56:27 +0200

    added some more methods

--- classes/Post.py
+++ classes/Post.py
@@ -5,13 +5,22 @@ from helpers import screen
 
 
 class Post:
+    username = ""
+    location = ""
+    description = ""
+    likes_counter = 0
+    comments_counter = 0
+    comments = []
+    showAll = False
+#----------------------------------------------------------------
     """
     A class used to represent post on Nitzagram
     """
-    def __init__(self): #TODO: add parameters
-        #TODO: write me!
-        pass
-
+    def __init__(self, username, location, description):
+        self.username = str(username)
+        self.location = str(location)
+        self.description = str(description)
+#----------------------------------------------------------------
     def display(self):
         """
         Display the Post image/Text, description, location, likes and comments
@@ -20,9 +29,8 @@ class Post:
         :return: None
         """
         # TODO: write me!
-        pass
 
-
+#----------------------------------------------------------------
     def display_comments(self):
         """
         Display comments on post. In case there are more than 4
@@ -30,24 +38,24 @@ class Post:
 
         :return: None
         """
+
         position_index = self.comments_display_index
         # If there are more than 4 comments, print "view more comments"
         if len(self.comments) > NUM_OF_COMMENTS_TO_DISPLAY:
-            comment_font = pygame.font.SysFont('chalkduster.ttf',
-                                               COMMENT_TEXT_SIZE)
-            view_more_comments_button = comment_font.render("view more comments",
-                                                            True, LIGHT_GRAY)
-            screen.blit(view_more_comments_button, (VIEW_MORE_COMMENTS_X_POS,
-                                                    VIEW_MORE_COMMENTS_Y_POS))
+            comment_font = pygame.font.SysFont('chalkduster.ttf',COMMENT_TEXT_SIZE)
+            view_more_comments_button = comment_font.render("view more comments",True, LIGHT_GRAY)
+            screen.blit(view_more_comments_button, (VIEW_MORE_COMMENTS_X_POS,VIEW_MORE_COMMENTS_Y_POS))
+    # -------------------------------------------------------------------------------------------------
 
         # Display 4 comments starting from comments_display_index
         for i in range(0, len(self.comments)):
             if position_index >= len(self.comments):
                 position_index = 0
+            # --------------------------------
             self.comments[position_index].display(i)
             position_index += 1
+            # --------------------------------
             if i >= NUM_OF_COMMENTS_TO_DISPLAY - 1:
                 break
-
-
-
+#----------------------------------------------------------------
+#----------------------------------------------------------------
